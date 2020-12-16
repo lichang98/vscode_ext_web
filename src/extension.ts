56dbb32410f0543a7cd6bb569861a0ca0dc78e7a
lichang98 <15365811995@163.com>
2020-12-17 06:21:24 +0800

    add post message from extension to webview

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as path from 'path'
-import * as fs from 'fs'
-import * as child_process from "child_process"
+import * as path from 'path';
+import * as fs from 'fs';
+import * as child_process from "child_process";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,7 +13,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "darwin2" is now active!');
 	// track current webview panel
-	let currentPanel:vscode.WebviewPanel | undefined = undefined
+	let currentPanel:vscode.WebviewPanel | undefined = undefined;
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -24,18 +24,18 @@ export function activate(context: vscode.ExtensionContext) {
 		if(currentPanel){
 			currentPanel.reveal(columnToShowIn);
 		}else{
-			currentPanel = vscode.window.createWebviewPanel("darwin2web", "Darwin IDE",vscode.ViewColumn.One,{localResourceRoots:[vscode.Uri.file(context.extensionPath)], enableScripts:true});
+			currentPanel = vscode.window.createWebviewPanel("darwin2web", "Darwin IDE",vscode.ViewColumn.One,{localResourceRoots:[vscode.Uri.file(context.extensionPath)], enableScripts:true,retainContextWhenHidden:true});
 			const onDiskPath = vscode.Uri.file(path.join(context.extensionPath,"src","resources","mainpage.html"));
 			let val = onDiskPath.with({scheme: "vscode-resource"});
 			let pathStr = val.toString();
-			console.log("pathStr:"+pathStr);
+			// console.log("pathStr:"+pathStr);
 			pathStr="C:\\Users\\32344\\Downloads\\darwin2\\src\\resources\\mainpage.html";
 			// fs.readFile(pathStr,'UTF-8',(err,fcontent)=>{
 			// 	console.log("file content:\n"+fcontent.toString());
 			// 	console.log("error:"+err?.message);
 			// });
 			let buf = fs.readFileSync(pathStr,"UTF-8");
-			console.log("Sync read: "+buf.toString());
+			// console.log("Sync read: "+buf.toString());
 			currentPanel.webview.html = buf.toString();
 			currentPanel.webview.onDidReceiveMessage(message =>{
 				console.log("extension receive msg: "+message);
@@ -44,6 +44,7 @@ export function activate(context: vscode.ExtensionContext) {
 			let pyScript = child_process.spawn("python",['C:\\Users\\32344\\Downloads\\darwin2\\test.py']);
 			pyScript.stdout.on("data",(data)=>{
 				console.log("python execued output:"+data);
+				currentPanel?.webview.postMessage({"data":data.toString()});
 			});
 
 

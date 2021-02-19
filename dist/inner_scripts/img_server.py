40c61c12b9c9d23a370461610d6980cd803f38e9
lichang98 <15365811995@163.com>
2021-02-19 08:32:44 +0800

    change for local sqlite db

--- dist/inner_scripts/img_server.py
+++ dist/inner_scripts/img_server.py
@@ -71,21 +71,22 @@ class SNNModelTable(declarative_base(metadata=metadata)):
     __tablename__ = "snn_model"
     id=sqlalchemy.Column("id",sqlalchemy.Integer, primary_key=true,autoincrement=True)
     snn_name = sqlalchemy.Column("snn_name",sqlalchemy.String(50))
+    snn_type = sqlalchemy.Column("snn_type", sqlalchemy.String(20))
     darwinlang_files = sqlalchemy.Column("darwinlang_files",sqlalchemy.String(1000))
     darwin_bin_files = sqlalchemy.Column("darwin_bin_files", sqlalchemy.String(1000))
+    extra_info = sqlalchemy.Column("extra_info", sqlalchemy.String(1000))
 
     def __repr__(self):
-        return "[SNN Row: id={}, snn_name={}, darwinlang_files={}, darwin_bin_files={}]".format(self.id, self.snn_name, self.darwinlang_files, self.darwin_bin_files)
+        return "[SNN Row: id={}, snn_name={}, snn_type={} darwinlang_files={}, darwin_bin_files={}, extra_info={}]".format(self.id, self.snn_name, self.snn_type, self.darwinlang_files, self.darwin_bin_files, self.extra_info)
 
-# curl -X POST -H "Content-Type: text/json" -d 
-# "{\"snn_name\": \"non test snn\", \"darwinlang_files\": \"non darlang files\", \"darwin_bin_files\": \"non bin files\"}" http://localhost:6003/db/post_snn_model
+# curl -X POST -H "Content-Type: text/json" -d "{\"snn_name\": \"non test snn\",\"snn_type\":\"classification\", \"darwinlang_files\": [\"C:\\model_pool\\darwinlang.json\"], \"darwin_bin_files\": [\"C:\\model_pool\\config.txt\"], \"extra_info\":\"\"}" http://localhost:6003/db/post_snn_model -i
 @app.route("/db/post_snn_model", methods=['POST'])
 def post_snn_model():
     data = request.get_data().decode("utf-8")
     print("receive post data={}".format(data))
     data = json.loads(data)
     sess = sqlalchemy.orm.sessionmaker(bind=engine)()
-    new_snn = SNNModelTable(snn_name=data["snn_name"], darwinlang_files=data["darwinlang_files"], darwin_bin_files=data["darwin_bin_files"])
+    new_snn = SNNModelTable(snn_name=data["snn_name"], snn_type=data["snn_type"],darwinlang_files=str(data["darwinlang_files"]), darwin_bin_files=str(data["darwin_bin_files"]), extra_info = str(data["extra_info"]))
     sess.add(new_snn)
     sess.commit()
     return Response()
@@ -103,13 +104,16 @@ def query_model_by_name():
     for row in query_res:
         resp_res.append({
             "snn_name": row.snn_name,
+            "snn_type": row.snn_type,
             "darwinlang_files": row.darwinlang_files,
-            "darwin_bin_files": row.darwin_bin_files
+            "darwin_bin_files": row.darwin_bin_files,
+            "extra_info": row.extra_info
         })
     return Response(json.dumps(resp_res), mimetype="text/javascript")
 
 
 if __name__ == "__main__":
+    metadata.create_all(engine)
     if not isUse("127.0.0.1",6003):
         app.run(host="0.0.0.0", port=6003)
         
